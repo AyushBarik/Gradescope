73373e11bad63727fd69df066406db36bb6c245a
AyushBarik <coolayush2017@gmail.com>
2024-05-15 18:37:43 +0530

    experiment

---
+++ image_classifier/__main__.py
@@ -0,0 +1,18 @@
+from urllib.request import urlopen
+import numpy as np
+import sys
+
+def main():
+    print("X")
+if __name__ == "__main__":
+    main()
+    
+    
+
+    print(sys.argv) 
+    # stores a list of arguments when python main.py abc
+
+    m = 4
+    n = 79
+    m = n
+    print(m)
